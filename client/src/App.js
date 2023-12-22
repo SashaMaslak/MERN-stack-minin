b007840ad5cd7166adbb7821f26b66126afa3d53
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-12-22 15:33:15 +0200

    add Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
+import "materialize-css"
+import AuthPage from "./pages/AuthPage"
+import CreatePage from "./pages/CreatePage"
+import DetailPage from "./pages/DetailPage"
+import LinksPage from "./pages/LinksPage"
+
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>REACT</h1>
     </div>
   )
